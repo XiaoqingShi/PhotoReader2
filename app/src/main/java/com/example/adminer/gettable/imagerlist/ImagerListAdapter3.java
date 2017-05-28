0e02700c43430d20863f8c7f50babb044eb546e7
董文雄 <994338972@qq.com>
2017-05-28 08:29:04 +0800

    1

--- app/src/main/java/com/example/adminer/gettable/imagerlist/ImagerListAdapter3.java
+++ app/src/main/java/com/example/adminer/gettable/imagerlist/ImagerListAdapter3.java
@@ -130,7 +130,7 @@ public class ImagerListAdapter3 extends BaseAdapter implements AbsListView.OnScr
         ImageView imageview;
     }
 
-    private void startLoadImages(int first, int total) {
+    private void startLoadImages(int first, int totael) {
         Log.v(TAG, "imagedown--->startLoadImages,first-->" + first + ",total-->" + total);
         for (int i = first; i < first + total; i++) {
             ImageUtils.loadImage(imageThumbUrls[i], listView);
